677962db23f8eaeb967ab481a470672b8e6dff69
Wim Looman <ghostunderscore@gmail.com>
2010-08-01 20:51:54 +1200

    Updating spacing

--- python/geom3.py
+++ python/geom3.py
@@ -1,15 +1,15 @@
 """Geometry module for use with COSC363, 2009.
    Defines Vector3, Point3, Line3, and Ray3 classes for
    3D geometry. Emphasis is on clarity and simplicity for
-    teaching purposes, not performance."""
+  teaching purposes, not performance."""
    
 from math import sqrt
 
 epsilon = 1.e-10  # Default epsilon for equality testing of points and vectors
 
 class GeomException(Exception):
-    def __init__(self, message = None):
-        Exception.__init__(self, message)
+  def __init__(self, message = None):
+    Exception.__init__(self, message)
 
 #================================================================
 #
@@ -18,61 +18,61 @@ class GeomException(Exception):
 #================================================================
 
 class Point3(object):
-    """Represents a Point in 3-space with coordinates x, y, z.
-    Note the distinction between vectors and points.
-    Points cannot, for example, be added or scaled."""
+  """Represents a Point in 3-space with coordinates x, y, z.
+  Note the distinction between vectors and points.
+  Points cannot, for example, be added or scaled."""
+  
+  def __init__(self, x, y=None, z=None):
+    """Constructor takes a Point3, a Vector3, a 3-tuple or
+    a 3-list or any other 3-sequence as a sole argument, or
+    values x, y and z."""
+    if y is None and z is None:
+      self.x, self.y, self.z = x   # Unpack a 3-sequence into coords 
+    else:
+      self.x, self.y, self.z = x, y, z  # Constructor taking x, y, z
+
+  def __sub__(self, other):
+    """P1 - P2 returns a vector. P - v returns a point"""
+    if isinstance(other, Point3):
+      return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
+    elif isinstance(other, Vector3):
+      return Point3(self.x - other.dx, self.y - other.dy, self.z - other.dz)
+    else:
+      return NotImplemented
     
-    def __init__(self, x, y=None, z=None):
-        """Constructor takes a Point3, a Vector3, a 3-tuple or
-        a 3-list or any other 3-sequence as a sole argument, or
-        values x, y and z."""
-        if y is None and z is None:
-            self.x, self.y, self.z = x   # Unpack a 3-sequence into coords 
-        else:
-            self.x, self.y, self.z = x, y, z  # Constructor taking x, y, z
+  def __add__(self, other):
+    """P + v is P translated by v"""
+    if isinstance(other, Vector3):
+      return Point3(self.x + other.dx, self.y + other.dy, self.z + other.dz)
+    else:
+      return NotImplemented
 
-    def __sub__(self, other):
-        """P1 - P2 returns a vector. P - v returns a point"""
-        if isinstance(other, Point3):
-            return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
-        elif isinstance(other, Vector3):
-            return Point3(self.x - other.dx, self.y - other.dy, self.z - other.dz)
-        else:
-            return NotImplemented
-        
-    def __add__(self, other):
-        """P + v is P translated by v"""
-        if isinstance(other, Vector3):
-            return Point3(self.x + other.dx, self.y + other.dy, self.z + other.dz)
-        else:
-            return NotImplemented
+  def __iter__(self):
+    """Iterator over the coordinates"""
+    return [self.x, self.y, self.z].__iter__()
+  
+  def __eq__(self, other):
+    """Equality of points is equality of all coordinates to within 
+     epsilon (defaults to 1.e-10)."""
+    return (abs(self.x - other.x) < epsilon and
+         abs(self.y - other.y) < epsilon and
+         abs(self.z - other.z) < epsilon)
 
-    def __iter__(self):
-        """Iterator over the coordinates"""
-        return [self.x, self.y, self.z].__iter__()
-    
-    def __eq__(self, other):
-        """Equality of points is equality of all coordinates to within 
-       epsilon (defaults to 1.e-10)."""
-        return (abs(self.x - other.x) < epsilon and
-                 abs(self.y - other.y) < epsilon and
-                 abs(self.z - other.z) < epsilon)
+  def __ne__(self, other):
+    """Inequality of points is inequality of any coordinates"""
+    return not self.__eq__(other)
+  
+  def __getitem__(self, i):
+    """P[i] is x, y, z for i in 0, 1, 2 resp."""
+    return [self.x, self.y, self.z][i]
 
-    def __ne__(self, other):
-        """Inequality of points is inequality of any coordinates"""
-        return not self.__eq__(other)
-    
-    def __getitem__(self, i):
-        """P[i] is x, y, z for i in 0, 1, 2 resp."""
-        return [self.x, self.y, self.z][i]
+  def __str__(self):
+    """String representation of a point"""
+    return ("(%.3f,%.3f,%.3f)") % (self.x, self.y, self.z)
 
-    def __str__(self):
-        """String representation of a point"""
-        return ("(%.3f,%.3f,%.3f)") % (self.x, self.y, self.z)
-
-    def __repr__(self):
-        """String representation including class"""
-        return "Point3" + str(self)
+  def __repr__(self):
+    """String representation including class"""
+    return "Point3" + str(self)
 
 #================================================================
 #
@@ -81,98 +81,98 @@ class Point3(object):
 #================================================================
 
 class Vector3(object):
-    """Represents a vector in 3-space with coordinates dx, dy, dz."""
-    
-    def __init__(self, dx, dy=None, dz=None):
-        """Constructor takes a Point3, a Vector3, a 3-tuple or
-        a 3-list or any other 3-sequence as a sole argument, or
-        values dx, dy and dz."""
-        if dy is None and dz is None:
-            self.dx, self.dy, self.dz = dx  # Constructor taking pt, vec, list or tuple as arg
-        else:
-            self.dx, self.dy, self.dz = dx, dy, dz  # Constructor taking x, y, z
+  """Represents a vector in 3-space with coordinates dx, dy, dz."""
+  
+  def __init__(self, dx, dy=None, dz=None):
+    """Constructor takes a Point3, a Vector3, a 3-tuple or
+    a 3-list or any other 3-sequence as a sole argument, or
+    values dx, dy and dz."""
+    if dy is None and dz is None:
+      self.dx, self.dy, self.dz = dx  # Constructor taking pt, vec, list or tuple as arg
+    else:
+      self.dx, self.dy, self.dz = dx, dy, dz  # Constructor taking x, y, z
 
-                                    
-    def __sub__(self, other):
-        """Vector difference"""
-        return Vector3(self.dx-other.dx, self.dy-other.dy, self.dz-other.dz)
-
-
-    def __add__(self, other):
-        """Vector sum"""
-        return Vector3(self.dx+other.dx, self.dy+other.dy, self.dz+other.dz)
+                  
+  def __sub__(self, other):
+    """Vector difference"""
+    return Vector3(self.dx-other.dx, self.dy-other.dy, self.dz-other.dz)
 
 
-    def __mul__(self, scale):
-        """v * r for r a float is scaling of vector v by r"""
-        return Vector3(scale*self.dx, scale*self.dy, scale*self.dz)
+  def __add__(self, other):
+    """Vector sum"""
+    return Vector3(self.dx+other.dx, self.dy+other.dy, self.dz+other.dz)
 
 
-    def __rmul__(self, scale):
-        """r * v for r a float is scaling of vector v by r"""
-        return self.__mul__(scale)
+  def __mul__(self, scale):
+    """v * r for r a float is scaling of vector v by r"""
+    return Vector3(scale*self.dx, scale*self.dy, scale*self.dz)
 
 
-    def __div__(self, scale):
-        """Division of a vector by a float r is scaling by (1/r)"""
-        return self.__mul__(1.0/scale)
-
-    def __neg__(self):
-        """Negation of a vector is negation of all its coordinates"""
-        return Vector3(-self.dx, -self.dy, -self.dz)
+  def __rmul__(self, scale):
+    """r * v for r a float is scaling of vector v by r"""
+    return self.__mul__(scale)
 
 
-    def __iter__(self):
-        """Iterator over coordinates dx, dy, dz in turn"""
-        return [self.dx, self.dy, self.dz].__iter__()
+  def __div__(self, scale):
+    """Division of a vector by a float r is scaling by (1/r)"""
+    return self.__mul__(1.0/scale)
+
+  def __neg__(self):
+    """Negation of a vector is negation of all its coordinates"""
+    return Vector3(-self.dx, -self.dy, -self.dz)
 
 
-    def __getitem__(self, i):
-        """v[i] is dx, dy, dz for i in 0,1,2 resp"""
-        return [self.dx, self.dy, self.dz][i]
-
-    def __eq__(self, other):
-        """Equality of vectors is equality of all coordinates to within 
-       epsilon (defaults to 1.e-10)."""
-        return (abs(self.dx - other.dx) < epsilon and
-                 abs(self.dy - other.dy) < epsilon and
-                 abs(self.dz - other.dz) < epsilon)
-
-    def __ne__(self, other):
-        """Inequality of vectors is inequality of any coordinates"""
-        return not self.__eq__(other)
-    
-    
-    def dot(self, other):
-        """The usual dot product"""
-        return self.dx*other.dx + self.dy*other.dy + self.dz*other.dz
+  def __iter__(self):
+    """Iterator over coordinates dx, dy, dz in turn"""
+    return [self.dx, self.dy, self.dz].__iter__()
 
 
-    def cross(self, other):
-        """The usual cross product"""
-        return Vector3(self.dy * other.dz - self.dz * other.dy,
-                      self.dz * other.dx - self.dx * other.dz,
-                      self.dx * other.dy - self.dy * other.dx) 
+  def __getitem__(self, i):
+    """v[i] is dx, dy, dz for i in 0,1,2 resp"""
+    return [self.dx, self.dy, self.dz][i]
+
+  def __eq__(self, other):
+    """Equality of vectors is equality of all coordinates to within 
+     epsilon (defaults to 1.e-10)."""
+    return (abs(self.dx - other.dx) < epsilon and
+         abs(self.dy - other.dy) < epsilon and
+         abs(self.dz - other.dz) < epsilon)
+
+  def __ne__(self, other):
+    """Inequality of vectors is inequality of any coordinates"""
+    return not self.__eq__(other)
+  
+  
+  def dot(self, other):
+    """The usual dot product"""
+    return self.dx*other.dx + self.dy*other.dy + self.dz*other.dz
 
 
-    def norm(self):
-        """A normalised version of self"""
-        return self/length(self)
+  def cross(self, other):
+    """The usual cross product"""
+    return Vector3(self.dy * other.dz - self.dz * other.dy,
+            self.dz * other.dx - self.dx * other.dz,
+            self.dx * other.dy - self.dy * other.dx) 
 
 
-    def unit(self):
-        """Same as 'norm'. Provided for compatibility with Visual"""
-        return norm(self)
-    
-    
-    def __str__(self):
-        """Minimal string representation in parentheses"""
-        return ("(%.3f,%.3f,%.3f)") % (self.dx, self.dy, self.dz)
+  def norm(self):
+    """A normalised version of self"""
+    return self/length(self)
 
 
-    def __repr__(self):
-        """String representation with class included"""
-        return "Vector3" + str(self)
+  def unit(self):
+    """Same as 'norm'. Provided for compatibility with Visual"""
+    return norm(self)
+  
+  
+  def __str__(self):
+    """Minimal string representation in parentheses"""
+    return ("(%.3f,%.3f,%.3f)") % (self.dx, self.dy, self.dz)
+
+
+  def __repr__(self):
+    """String representation with class included"""
+    return "Vector3" + str(self)
 
 #================================================================
 #
@@ -181,48 +181,48 @@ class Vector3(object):
 #================================================================
 
 class Line3(object):
-    """A Line3 is defined by two points in space"""
+  """A Line3 is defined by two points in space"""
+  
+  def __init__(self, p1, p2):
+    """Constructor takes two points (or anything convertible to Point3)"""
+    self.p1 = Point3(p1)
+    self.p2 = Point3(p2)
     
-    def __init__(self, p1, p2):
-        """Constructor takes two points (or anything convertible to Point3)"""
-        self.p1 = Point3(p1)
-        self.p2 = Point3(p2)
-        
-    def pos(self, alpha):
-        """The position p1 + alpha*(p2-p1) on the Line3"""
-        return self.p1 + alpha * (self.p2-self.p1)
-    
-    def repr(self):
-        """String representation of a Line3"""
-        return "Line3(%.3g, %.3g)" % (p1, p2)
+  def pos(self, alpha):
+    """The position p1 + alpha*(p2-p1) on the Line3"""
+    return self.p1 + alpha * (self.p2-self.p1)
+  
+  def repr(self):
+    """String representation of a Line3"""
+    return "Line3(%.3g, %.3g)" % (p1, p2)
 
 #================================================================
 #
 # Ray3 class
 #
 #================================================================
-    
+  
 class Ray3(object):
-    """A Ray3 is a directed Line3, defined by a start point and a direction"""
-    
-    def __init__(self, start, dir):
-        """Constructor takes a start point (or something convertible to point) and 
-          a direction vector"""
-        self.start = Point3(start)     # Ensure start point represented as a Point3
-        self.dir = unit(Vector3(dir))  # Direction vector
+  """A Ray3 is a directed Line3, defined by a start point and a direction"""
+  
+  def __init__(self, start, dir):
+    """Constructor takes a start point (or something convertible to point) and 
+      a direction vector"""
+    self.start = Point3(start)   # Ensure start point represented as a Point3
+    self.dir = unit(Vector3(dir))  # Direction vector
 
-    def pos(self, alpha):
-        """A point on a Ray3 is start + alpha*dir for alpha positive."""
-        if alpha >= 0:
-            return self.start + alpha * self.dir
-        else:
-            raise GeomException("Attempt to obtain point not on Ray3")
+  def pos(self, alpha):
+    """A point on a Ray3 is start + alpha*dir for alpha positive."""
+    if alpha >= 0:
+      return self.start + alpha * self.dir
+    else:
+      raise GeomException("Attempt to obtain point not on Ray3")
 
-    def __repr__(self):
-        return "Ray3(%s,%s)" % (str(self.start), str(self.dir))
-    
-    
-    
+  def __repr__(self):
+    return "Ray3(%s,%s)" % (str(self.start), str(self.dir))
+  
+  
+  
 #================================================================
 #
 # Global functions on points and vectors
@@ -230,25 +230,25 @@ class Ray3(object):
 #================================================================
 
 def dot(v1, v2):
-    """Dot product of two vectors"""
-    return v1.dot(v2)
+  """Dot product of two vectors"""
+  return v1.dot(v2)
 
 def cross(v1, v2):
-    """Cross product of two vectors"""
-    return v1.cross(v2)
+  """Cross product of two vectors"""
+  return v1.cross(v2)
 
 def length(v):
-    """Length of vector"""
-    return sqrt(v.dot(v))
+  """Length of vector"""
+  return sqrt(v.dot(v))
 
 def unit(v):
-    """A unit vector in the direction of v"""
-    return v / length(v)
+  """A unit vector in the direction of v"""
+  return v / length(v)
 
 def norm(v):
-    """A unit vector in the direction of v.
-       Provided for compatibility with Visual"""
-    return unit(v)
+  """A unit vector in the direction of v.
+     Provided for compatibility with Visual"""
+  return unit(v)
 
 #================================================================
 #
@@ -256,56 +256,56 @@ def norm(v):
 #
 #================================================================
 if __name__ == '__main__':
-    
-    # Simple tests of all basic vector operations
-    
-    v1 = Vector3(1,2,3)
-    v2 = Vector3(3,2,1)
-    assert Vector3((1,2,3)) == v1
-    assert Vector3([1,2,3]) == v1
-    assert Vector3(Point3(1,2,3)) == v1
-    assert v1 + v2 == Vector3(4,4,4)
-    assert v1 - v2 == Vector3(-2,0,2)
-    assert v1 * 3 == Vector3(3,6,9)
-    assert 3 * v1 == Vector3(3,6,9)
-    assert v1/2.0 == Vector3(0.5,1,1.5)
-    assert -v1 == Vector3(-1,-2,-3)
-    assert v1[0] == 1 and v1[1] == 2 and v1[2] == 3
-    assert list(v1) == [1,2,3]
-    assert str(v1) == "(1.000,2.000,3.000)"
-    assert eval(repr(v1)) == v1
-    assert v1.dot(v2) == 10
-    assert v1.dot(v2) == dot(v1,v2)
-    assert v1.cross(v2) == Vector3(-4,8,-4)
-    assert length(unit(Vector3(2,3,4))) == 1.0
-    assert length(Vector3(2,3,4).norm()) == 1.0
-    
-    # Tests on points
-    
-    p1 = Point3(2,4,6)
-    p2 = Point3(4,7,3)
-    assert Point3((2,4,6)) == p1
-    assert Point3([2,4,6]) == p1
-    assert Point3(Vector3(2,4,6)) == p1
-    assert [p1[i] for i in range(3)] == [2,4,6]
-    assert p1-p2 == Vector3(-2,-3,3)
-    assert p1+v1 == Point3(3,6,9)
-    assert str(p1) == "(2.000,4.000,6.000)"
-    assert eval(repr(p1)) == p1
-    try:
-        p1 + p2
-        assert False
-    except TypeError: pass
-    try:
-        3 * p1
-        assert False
-    except TypeError: pass
+  
+  # Simple tests of all basic vector operations
+  
+  v1 = Vector3(1,2,3)
+  v2 = Vector3(3,2,1)
+  assert Vector3((1,2,3)) == v1
+  assert Vector3([1,2,3]) == v1
+  assert Vector3(Point3(1,2,3)) == v1
+  assert v1 + v2 == Vector3(4,4,4)
+  assert v1 - v2 == Vector3(-2,0,2)
+  assert v1 * 3 == Vector3(3,6,9)
+  assert 3 * v1 == Vector3(3,6,9)
+  assert v1/2.0 == Vector3(0.5,1,1.5)
+  assert -v1 == Vector3(-1,-2,-3)
+  assert v1[0] == 1 and v1[1] == 2 and v1[2] == 3
+  assert list(v1) == [1,2,3]
+  assert str(v1) == "(1.000,2.000,3.000)"
+  assert eval(repr(v1)) == v1
+  assert v1.dot(v2) == 10
+  assert v1.dot(v2) == dot(v1,v2)
+  assert v1.cross(v2) == Vector3(-4,8,-4)
+  assert length(unit(Vector3(2,3,4))) == 1.0
+  assert length(Vector3(2,3,4).norm()) == 1.0
+  
+  # Tests on points
+  
+  p1 = Point3(2,4,6)
+  p2 = Point3(4,7,3)
+  assert Point3((2,4,6)) == p1
+  assert Point3([2,4,6]) == p1
+  assert Point3(Vector3(2,4,6)) == p1
+  assert [p1[i] for i in range(3)] == [2,4,6]
+  assert p1-p2 == Vector3(-2,-3,3)
+  assert p1+v1 == Point3(3,6,9)
+  assert str(p1) == "(2.000,4.000,6.000)"
+  assert eval(repr(p1)) == p1
+  try:
+    p1 + p2
+    assert False
+  except TypeError: pass
+  try:
+    3 * p1
+    assert False
+  except TypeError: pass
 
-    print "Passed all tests"
-        
-        
+  print "Passed all tests"
     
     
-    
+  
+  
+  
 
 
--- python/rayCaster.py
+++ python/rayCaster.py
@@ -13,11 +13,11 @@ from datetime import datetime
 
 MULTI = 1 # Level of linear AA
 
-WIN_SIZE = 100                      # Screen window size (square)
-LIGHT_DIR = unit(Vector3(0.5,0.5,2))    # The direction vector towards the light source
-LIGHT_INTENS = 0.8                  # Intensity of the single white light source
-AMBIENT = 0.1                       # Ambient light level (assumed white light)
-BACKGROUND = Colour(0.6,0.6,0.6)    # Colour of the background
+WIN_SIZE = 100            # Screen window size (square)
+LIGHT_DIR = unit(Vector3(0.5,0.5,2))  # The direction vector towards the light source
+LIGHT_INTENS = 0.8          # Intensity of the single white light source
+AMBIENT = 0.1             # Ambient light level (assumed white light)
+BACKGROUND = Colour(0.6,0.6,0.6)  # Colour of the background
 
 SHINY_RED = Material(Colour(0.7, 0.3, 0.2), Colour(0.4,0.4,0.4), 100)
 SHINY_BLUE = Material(Colour(0.2, 0.3, 0.7), Colour(0.8,0.8,0.8), 200)
@@ -27,107 +27,107 @@ MATT_GREEN = Material(Colour(0.1, 0.7, 0.1), Colour(1,1,1), 100)
 # the View.eye_ray method has been rewritten.
 
 EYEPOINT = Point3(0.5, 0.5, 2)  # Where the eye is
-LOOKAT = Point3(0.5, 0.5, 0)    # The look at point
-FOV = 45                        # Field of view (degrees).
+LOOKAT = Point3(0.5, 0.5, 0)  # The look at point
+FOV = 45            # Field of view (degrees).
 
 
 class View(object):
-    '''A View specifies a camera's position and orientation in space,
-       plus its field_of_view (fov) in degrees. It also specifies the
-       required image size (width x height).'''
+  '''A View specifies a camera's position and orientation in space,
+     plus its field_of_view (fov) in degrees. It also specifies the
+     required image size (width x height).'''
 
+  
+  def __init__(self, eye_point, look_at, fov, width, height):
+    '''Constructor sets the eye-point, look-at-point and field of view,
+       plus the width and height (in pixels) of the image that will
+       be computed using this view.'''
     
-    def __init__(self, eye_point, look_at, fov, width, height):
-        '''Constructor sets the eye-point, look-at-point and field of view,
-           plus the width and height (in pixels) of the image that will
-           be computed using this view.'''
-        
-        self.eye_point = eye_point
-        self.look_at = look_at
-        self.fov = fov
-        self.width = width
-        self.height = height
-    
-    def eye_rays(self, row, col):
-        rays = []
-        spacing = 1.0 / self.width
-        for irow in range(MULTI):
-            for icol in range(MULTI):
-                y = (self.height * MULTI - (row * MULTI + irow)) * spacing / MULTI
-                x = (col * MULTI + icol) * spacing / MULTI
-                rays.append(Ray3(self.eye_point, Point3(x, y, 1) - self.eye_point))
-        return rays
+    self.eye_point = eye_point
+    self.look_at = look_at
+    self.fov = fov
+    self.width = width
+    self.height = height
+  
+  def eye_rays(self, row, col):
+    rays = []
+    spacing = 1.0 / self.width
+    for irow in range(MULTI):
+      for icol in range(MULTI):
+        y = (self.height * MULTI - (row * MULTI + irow)) * spacing / MULTI
+        x = (col * MULTI + icol) * spacing / MULTI
+        rays.append(Ray3(self.eye_point, Point3(x, y, 1) - self.eye_point))
+    return rays
 
-    
+  
 
 # ======= A Ray Tracing/Ray Casting Camera class ========= 
 
 class Camera(object):
-    '''An Camera provides a 'take_photo' method that computes the ray-casting 
-       image of a given scene from a given view with given lighting.'''
+  '''An Camera provides a 'take_photo' method that computes the ray-casting 
+     image of a given scene from a given view with given lighting.'''
 
-    def __init__(self, view, scene, lighting):
-        self.view = view
-        self.scene = scene
-        self.lighting = lighting
+  def __init__(self, view, scene, lighting):
+    self.view = view
+    self.scene = scene
+    self.lighting = lighting
 
-    def colours_along_rays(self, rays):
-        colours = []
-        hitpoints = self.scene.intersectMultiple(rays)
-        for index in range(len(hitpoints)):
-            if hitpoints[index][1] == float('Inf'):
-                colours.append(BACKGROUND)
-            else:
-                (obj, alpha) = hitpoints[index]
-                try:colours.append(obj.material.lit_colour(obj.normal(rays[index].pos(alpha)), self.lighting, -rays[index].dir))
-                except GeomException:
-                  print obj
-                  print alpha
-                  print rays[index]
-                  
-        return colours
+  def colours_along_rays(self, rays):
+    colours = []
+    hitpoints = self.scene.intersectMultiple(rays)
+    for index in range(len(hitpoints)):
+      if hitpoints[index][1] == float('Inf'):
+        colours.append(BACKGROUND)
+      else:
+        (obj, alpha) = hitpoints[index]
+        try:colours.append(obj.material.lit_colour(obj.normal(rays[index].pos(alpha)), self.lighting, -rays[index].dir))
+        except GeomException:
+          print obj
+          print alpha
+          print rays[index]
+          
+    return colours
+  
+  
+  def colour_of_pixel(self, row, col):
+    colour = Colour(0,0,0)
+    for ray in self.view.eye_rays(row, col):
+      hitpoint = self.scene.intersect(ray)
+      if hitpoint[1] == float('Inf'):
+        colour += BACKGROUND
+      else:
+        (obj, alpha) = hitpoint
+        try:colour += obj.material.lit_colour(obj.normal(ray.pos(alpha)), self.lighting, -ray.dir)
+        except GeomException:
+          print obj
+          print alpha
+          print ray
+          
+    colour = colour / MULTI ** 2
+    return colour
+
+  def take_photo(self):
+    img = Image.new("RGB", (self.view.width, self.view.height))
     
+    for row in range(self.view.height):
+      for col in range(self.view.width):
+        pixel = self.colour_of_pixel(row, col)
+        img.putpixel((col, row), pixel.intColour())
+        
+    return img
+
     
-    def colour_of_pixel(self, row, col):
-        colour = Colour(0,0,0)
-        for ray in self.view.eye_rays(row, col):
-            hitpoint = self.scene.intersect(ray)
-            if hitpoint[1] == float('Inf'):
-                colour += BACKGROUND
-            else:
-                (obj, alpha) = hitpoint
-                try:colour += obj.material.lit_colour(obj.normal(ray.pos(alpha)), self.lighting, -ray.dir)
-                except GeomException:
-                  print obj
-                  print alpha
-                  print ray
-                  
-        colour = colour / MULTI ** 2
-        return colour
-
-    def take_photo(self):
-        img = Image.new("RGB", (self.view.width, self.view.height))
-        
-        for row in range(self.view.height):
-            for col in range(self.view.width):
-                pixel = self.colour_of_pixel(row, col)
-                img.putpixel((col, row), pixel.intColour())
-                
-        return img
-
-        
 # ====== Main body. Compute and display image ========
 
 start = datetime.now()
 lighting = Lighting(LIGHT_INTENS, LIGHT_DIR, AMBIENT)
 
 scene = Scene([Sphere(Point3(0.35,0.6,0.5), 0.25, SHINY_BLUE),
-               Sphere(Point3(0.75,0.2,0.6), 0.15, SHINY_RED),
-               Plane(Point3(0.5,0.5,0), Vector3(0,0,1), MATT_GREEN)])
+         Sphere(Point3(0.75,0.2,0.6), 0.15, SHINY_RED),
+         Plane(Point3(0.5,0.5,0), Vector3(0,0,1), MATT_GREEN)])
 
 view = View(EYEPOINT, LOOKAT, FOV, WIN_SIZE, WIN_SIZE)
 camera = Camera(view, scene, lighting)
 
 img = camera.take_photo()
-img.save('image.bmp')      # Display image in default image-viewer application
+img.save('image.bmp')    # Display image in default image-viewer application
 print "Rendering time:", datetime.now() - start
