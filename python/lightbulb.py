d3839141dc65b772f36bceadbd7db188a636ca77
Wim Looman <ghostunderscore@gmail.com>
2010-08-03 12:48:17 +1200

    Missed file

---
+++ python/lightbulb.py
@@ -0,0 +1,21 @@
+from material import *
+from lighting import PointLight
+from shapes import Sphere
+from math import sqrt
+
+class LightBulb(object):
+  def __init__(self, colour, point, size, scene):
+    light = PointLight(colour, point, scene)
+    material = LightBulbMaterial(light)
+    bulb = Sphere(point, size, material, scene)
+
+
+class LightBulbMaterial(Material):
+  def __init__(self, light):
+    super(LightBulbMaterial, self).__init__(None)
+    self.casts_shadow = False
+    self.light = light
+    self.addition = sqrt(light.value.r ** 2 + light.value.g ** 2 + light.value.b ** 2)
+    
+  def lit_colour(self, scene, normal, view_vector, point):
+    return (view_vector.dot(normal) ** 2) * self.light.diffuseLighting(-normal, point, scene) + Colour(0.5, 0.5, 0.5)
